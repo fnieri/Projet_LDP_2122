177ac20ece9c3c5bf7bbdf3f312eb9d01e25617c
fnieri <francesco.nieri@ulb.be>
2021-11-15 20:43:00 +0100

    Added sprites

---
+++ src/classes/animation.hpp
@@ -0,0 +1,4 @@
+#ifndef __ANIMATION_HPP
+#define __ANIMATION_HPP
+
+#endif
---
+++ src/classes/candyspeciality.hpp
@@ -0,0 +1,9 @@
+#ifndef __CANDYSPECIALITY_HPP
+#define __CANDYSPECIALITY_HPP
+
+class candySpeciality {
+    private:
+        int value;
+    public:
+};
+#endif
