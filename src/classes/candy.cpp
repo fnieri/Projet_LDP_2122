2921166a16f99d31a442e2068d193efd9cb827f9
fnieri <francesco.nieri@ulb.be>
2021-11-16 00:02:39 +0100

    Added and defined Candy Methods

--- src/classes/candy.cpp
+++ src/classes/candy.cpp
@@ -1,1 +1,60 @@
- 
+#include <candy.hpp>
+
+Candy::Candy (Color color) : color{color} {};
+
+Candy::~Candy() = default;
+
+Color Candy::getColor() {
+    return this->color;
+}
+
+void Candy::setColor(Color color){
+    this->color = color;
+}
+
+void Candy::setImages(){
+    Color randomColor = static_cast<Color>(rand() % NUMBER_OF_COLORS);
+    std::string candyImagePrefix = generateCandyImagePrefix(randomColor);
+    
+    this->setColor(randomColor);
+    this->setIdleImage(candyImagePrefix);
+    this->setOnHoverImage(candyImagePrefix);
+}
+
+std::string Candy::generateCandyImagePrefix(Color color) {
+    std::string returnPrefix;
+    switch (color)
+    {
+    
+        case Color::RED:
+            returnPrefix == "red";
+            break;
+        case Color::ORANGE:
+            returnPrefix == "orange";
+            break;
+        case Color::YELLOW:
+            returnPrefix == "yellow";
+            break;
+        case Color::GREEN:
+            returnPrefix == "blue";
+            break;
+        case Color::BLUE:
+            returnPrefix == "blue";
+            break;
+        case Color::PURPLE:
+            returnPrefix == "purple";
+            break;
+        default:
+            break;
+    }
+
+   return returnPrefix;
+
+}
+/*
+void Candy::setIdleImage(std::string prefix){
+    std::string idleImageName = prefix + CANDY_IDLE_IMAGE_EXTENSION;
+    this->candyIdleImage = new Fl_PNG_Image(WORKING_PATH);
+
+}
+*/
